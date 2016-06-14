e2bbc71e4f475ed348a4073a3e6ecfb32b2115c1
Christopher Ahmed <Christopher.Ahmed@intelematics.com>
2016-06-14 16:31:42 -0700

    restructure controllers

--- server/boot/routes.js
+++ server/boot/routes.js
@@ -5,20 +5,22 @@
 
 module.exports = function(app) {
 
+  var loginController = require('../controllers/loginController')(app);
   var homeController = require('../controllers/homeController')(app);
   var adminController = require('../controllers/adminController')(app);
   var photoController = require('../controllers/photoController')(app);
   var projectController = require('../controllers/projectController')(app);
   var workController = require('../controllers/workController')(app);
   var memberController =  require('../controllers/memberController')(app);
-  var lucaFGroupController =  require('../controllers/lucafGroupController')(app);
+  var lucafGroupController =  require('../controllers/lucafGroupController')(app);
 
-  app.use('/', homeController);
+  app.use('/', loginController);
+  app.use('/home', homeController);
   app.use('/admin', adminController);
   app.use('/photo', photoController);
   app.use('/project', projectController);
   app.use('/work', workController);
   app.use('/member', memberController);
-  app.use('/lucafgroup',lucaFGroupController);
+  app.use('/lucafgroup',lucafGroupController);
 
 };
