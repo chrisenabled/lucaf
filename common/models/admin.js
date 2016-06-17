9761933185852604ec9ee37b7eda4c4bab159a1d
Christopher Ahmed <Christopher.Ahmed@intelematics.com>
2016-06-16 17:04:29 -0700

    Configured Jade views and working email with jade template

--- common/models/admin.js
+++ common/models/admin.js
@@ -5,6 +5,8 @@
 
 var config = require('../../server/config.json');
 var path = require('path');
+var fs = require('fs');
+var jade = require('jade');
 
 module.exports = function(Admin) {
   //send verification email after registration
@@ -16,17 +18,17 @@ module.exports = function(Admin) {
       to: user.email,
       from: 'noreply@loopback.com',
       subject: 'Thanks for registering.',
-      template: path.resolve(__dirname, '../../server/views/verify.jade'),
+      template: path.resolve(__dirname, '../../server/views/login/verify.jade'),
       redirect: '/verified',
       user: user
     };
 
-    user.verify(options, function(err, response) {
+    user.verifyRegistration(options, function(err, response) {
       if (err) return next(err);
 
       console.log('> verification email sent:', response);
 
-      context.res.render('response', {
+      context.res.render('login/response', {
         title: 'Signed up successfully',
         content: 'Please check your email and click on the verification link ' +
             'before logging in.',
@@ -52,4 +54,96 @@ module.exports = function(Admin) {
       console.log('> sending password reset email to:', info.email);
     });
   });
+
+  // --------------------------Verification Prototype---------------------------------
+  Admin.prototype.verifyRegistration = function(options, fn) {
+    fn = fn || utils.createPromiseCallback();
+
+    var user = this;
+    var userModel = this.constructor;
+    var registry = userModel.registry;
+
+    options.redirect = options.redirect || '/';
+    options.template = path.resolve(options.template);
+    options.user = this;
+    options.protocol = options.protocol || 'http';
+
+    var app = userModel.app;
+    options.host = options.host || (app && app.get('host')) || 'localhost';
+    options.port = options.port || (app && app.get('port')) || 3000;
+    options.restApiRoot = options.restApiRoot || (app && app.get('restApiRoot')) || '/api';
+
+    var displayPort = (
+      (options.protocol === 'http' && options.port == '80') ||
+      (options.protocol === 'https' && options.port == '443')
+    ) ? '' : ':' + options.port;
+
+    options.verifyHref = options.verifyHref ||
+      options.protocol +
+      '://' +
+      options.host +
+      displayPort +
+      options.restApiRoot +
+      userModel.http.path +
+      userModel.sharedClass.find('confirm', true).http.path +
+      '?uid=' +
+      options.user.id +
+      '&redirect=' +
+      options.redirect;
+
+    // Email model
+    var Email = options.mailer || this.constructor.email || registry.getModelByType(loopback.Email);
+
+    // Set a default token generation function if one is not provided
+    var tokenGenerator = options.generateVerificationToken || Admin.generateVerificationToken;
+
+    tokenGenerator(user, function(err, token) {
+      if (err) { return fn(err); }
+
+      user.verificationToken = token;
+      user.save(function(err) {
+        if (err) {
+          fn(err);
+        } else {
+          sendEmail(user);
+        }
+      });
+    });
+
+    // compile jade file
+    function compileJade(file) {
+      var templates = this._templates || (this._templates = {});
+      var str = templates[file] || (templates[file] = fs.readFileSync(file, 'utf8'));
+      return jade.compile(str, {
+        filename: file
+      });
+    };
+
+    function sendEmail(user) {
+      options.verifyHref += '&token=' + user.verificationToken;
+
+      options.text = options.text || 'Please verify your email by opening this link in a web browser:\n\t{href}';
+
+      options.text = options.text.replace(/\{href\}/g, options.verifyHref);
+
+      options.to = options.to || user.email;
+
+      options.subject = options.subject || 'Thanks for Registering';
+
+      options.headers = options.headers || {};
+
+      var template = compileJade(options.template);
+      options.html = template(options);
+
+      Email.send(options, function(err, email) {
+        if (err) {
+          fn(err);
+        } else {
+          fn(null, {email: email, token: user.verificationToken, uid: user.id});
+        }
+      });
+    }
+    return fn.promise;
+  };
+  //----------------------------------------------------------------------------------
 };
--- server/middleware/requestLog.js
+++ server/middleware/requestLog.js
@@ -14,15 +14,21 @@ module.exports = function () {
     });
   }
   return function requestLog(req, res, next) {
-    var date = new Date(Date.now());
-    var options = {
-      weekday: "long", year: "numeric", month: "short", day: "numeric", hour: "2-digit", minute: "2-digit",
-      second: "2-digit", timeZone: 'UTC', timeZoneName : "short"
-    };
-    console.log('....................................');
-    console.log('Request headers: ', JSON.stringify(req.headers));
-    console.log('Url: ',fullUrl(req),' Time: ', date.toLocaleTimeString("en-us", options));
-    console.log('....................................');
-    next();
+    require('fs').stat(__dirname + req.url, function(err, stats) {
+      var isFile = stats && stats.isFile();
+      //do not process static files
+      if (isFile == false) {
+        var date = new Date(Date.now());
+        var options = {
+          weekday: "long", year: "numeric", month: "short", day: "numeric", hour: "2-digit", minute: "2-digit",
+          second: "2-digit", timeZone: 'UTC', timeZoneName : "short"
+        };
+        console.log('....................................');
+        console.log('Request headers: ', JSON.stringify(req.headers));
+        console.log('Url: ',fullUrl(req),' Time: ', date.toLocaleTimeString("en-us", options));
+        console.log('....................................');
+      }
+      next();
+    });
   };
 };
